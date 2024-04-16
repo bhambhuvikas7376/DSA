7c83ccfb26040d59b854de6432e2a26a6dd7ff7b
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-16 18:16:22 +0530

    8

---
+++ Java_Coding_Question_50/StringremovespacesAllMethod.java
@@ -0,0 +1,13 @@
+package Java_Coding_Question_50;
+
+public class StringremovespacesAllMethod {
+public static void main(String[] args) {
+    String s="  cmrit  ";
+
+    System.out.println(s.trim());
+    System.out.println(s.strip());
+    System.out.println(s.stripLeading());
+    System.out.println(s.stripTrailing());
+}
+    
+}
