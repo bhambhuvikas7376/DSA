2b74b7d2419485ef9c91cbd0ee273541c32cbed9
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-16 18:24:09 +0530

    10

---
+++ Java_Coding_Question_50/sortanarray.java
@@ -0,0 +1,9 @@
+package Java_Coding_Question_50;
+import java.util.*;
+public class sortanarray {
+    public static void main(String[] args) {
+        int[] ar={1,6,89,5,3,8};
+        Arrays.sort(ar); 
+        System.out.println(Arrays.toString(ar));
+    }
+}
