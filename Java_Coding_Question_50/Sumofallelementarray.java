6ad00fd58d0f21fa2f62a61750010199abf37f40
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-17 20:53:23 +0530

    18

---
+++ Java_Coding_Question_50/Sumofallelementarray.java
@@ -0,0 +1,15 @@
+package Java_Coding_Question_50;
+
+public class Sumofallelementarray {
+    public static void main(String[] args) {
+        int[] ar={1,2,3,4,5};
+        int sum=0;
+        for(int i=0;i<ar.length;i++)
+        {
+            sum+=ar[i];
+        }
+
+        System.out.println(sum);
+
+    }
+}
