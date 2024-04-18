232a62b9f956904458c49d7459b79dcd350c43b1
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-18 14:30:07 +0530

    21

---
+++ Java_Coding_Question_50/Findstringintext.java
@@ -0,0 +1,8 @@
+package Java_Coding_Question_50;
+
+public class Findstringintext {
+    public static void main(String[] args) {
+        String s="hello hello";
+        System.out.println(s.contains("ll"));
+    }
+}
