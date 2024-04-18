d0d56a51d0202b0a0cd01ea6b546c87d923af1ab
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-18 15:41:28 +0530

    22

---
+++ Java_Coding_Question_50/Dateformat.java
@@ -0,0 +1,16 @@
+package Java_Coding_Question_50;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Dateformat {
+    public static void main(String[] args) {
+        String pattern="dd-mm-yyyy";
+
+        SimpleDateFormat dateFormat=new SimpleDateFormat(pattern);
+        String date= dateFormat.format(new Date());
+
+        System.out.println(date);
+    }
+}
+
