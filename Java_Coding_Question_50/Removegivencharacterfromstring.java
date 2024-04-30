758df273ca7fbba74c3e8e41d886822912463f68
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-30 18:38:47 +0530

    25

---
+++ Java_Coding_Question_50/Removegivencharacterfromstring.java
@@ -0,0 +1,9 @@
+package Java_Coding_Question_50;
+
+public class Removegivencharacterfromstring {
+    public static void main(String[] args) {
+        String s="abkcnjebbshahvdhjdvfhbbjg";
+        s=s.replace("a", "");
+        System.out.println(s);
+    }
+}
